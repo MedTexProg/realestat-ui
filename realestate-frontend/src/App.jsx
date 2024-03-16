a4b22f9c36e1b2b27bfaf7ec0dd785821b576259
Medtex <medtexprog@gmail.com>
2024-03-16 23:42:12 +0800

    Initial commit

---
+++ realestate-frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import "./layout.scss"
+import Navbar from "./components/navbar/Navbar.jsx";
+import HomePage from "./routes/homePage/homePage.jsx";
+
+function App() {
+
+    return (
+        <div className="layout">
+            <div className="navbar">
+                <Navbar/>
+            </div>
+            <div className="content">
+                <HomePage/>
+            </div>
+        </div>
+    )
+}
+
+export default App
